ddfb47d291e44cc1263de7d688ffdb7ca9a81e64
LocPerseus <kinghoangloc@gmail.com>
2019-11-17 21:42:15 +0700

    show your cam

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use('/', (req, res) => {
+    res.render('home');
+})
+app.listen(port, () => console.log('Server started on port:', port));
