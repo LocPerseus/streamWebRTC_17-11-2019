092c5b43380d520ee1fc9ed6a79f7c072156c151
LocPerseus <kinghoangloc@gmail.com>
2019-11-18 10:26:18 +0700

    tach file

--- src/app.js
+++ src/app.js
@@ -1,2 +1,23 @@
 const openStream = require('./openStream');
-openStream();
+const playVideo = require("./playVideo");
+const Peer = require("simple-peer");
+const $ = require('jquery');
+openStream(function(stream) {
+    playVideo(stream, "localStream");
+    const p = new Peer({ initiator: location.hash === "#1", trickle: false, stream: stream }); // trickle sử dụng server bên ngoài
+    p.on("signal", token => {
+        $("#txtMySignal").val(JSON.stringify(token));
+    });
+    // p.on('connect', () => {
+    //     setInterval(() => p.send(Math.random()), 2000);
+    //     console.log('Connected')
+    // });
+    // p.on('data', data => console.log('NHAN DU LIEU: ' + data));
+
+    $("#btnConnect").click(() => {
+        const friendSignal = JSON.parse($("#txtFriendSignal").val());
+        p.signal(friendSignal);
+    });
+    p.on('stream', friendStream => playVideo(friendStream, 'friendStream'))
+    console.log("Hello World");
+});
--- src/openStream.js
+++ src/openStream.js
@@ -1,31 +1,13 @@
 const playVideo = require("./playVideo");
-const Peer = require("simple-peer");
-const $ = require('jquery');
 
-function openStream() {
+function openStream(cb) { // cb: callback
     navigator.mediaDevices
         .getUserMedia({
             audio: false,
             video: true
         })
         .then(stream => {
-            playVideo(stream, "localStream");
-            const p = new Peer({ initiator: location.hash === "#1", trickle: false, stream: stream }); // trickle sử dụng server bên ngoài
-            p.on("signal", token => {
-                $("#txtMySignal").val(JSON.stringify(token));
-            });
-            // p.on('connect', () => {
-            //     setInterval(() => p.send(Math.random()), 2000);
-            //     console.log('Connected')
-            // });
-            // p.on('data', data => console.log('NHAN DU LIEU: ' + data));
-
-            $("#btnConnect").click(() => {
-                const friendSignal = JSON.parse($("#txtFriendSignal").val());
-                p.signal(friendSignal);
-            });
-            p.on('stream', friendStream => playVideo(friendStream, 'friendStream'))
-            console.log("Hello World");
+            cb(stream); // khi có stream. gọi function cb
         })
         .catch(err => console.log(err));
 }
