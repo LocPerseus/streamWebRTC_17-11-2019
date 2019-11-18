465ea9a203c1239f0692e9ffc9ae850594f0ca2b
LocPerseus <kinghoangloc@gmail.com>
2019-11-18 09:58:39 +0700

    xong connect send

--- src/app.js
+++ src/app.js
@@ -1,4 +1,19 @@
-const openCamera = require('./openCamera');
+const openStream = require('./openStream');
 
-openCamera();
-console.log('Xin chao cac ban');
+const $ = require('jquery');
+// openStream();
+const Peer = require('simple-peer');
+const p = new Peer({ initiator: location.hash === '#1', trickle: false }); // trickle sử dụng server bên ngoài
+p.on('signal', token => {
+    $('#txtMySignal').val(JSON.stringify(token));
+});
+p.on('connect', () => {
+    setInterval(() => p.send(Math.random()), 2000);
+    console.log('Connected')
+});
+p.on('data', data => console.log('NHAN DU LIEU: ' + data));
+$('#btnConnect').click(() => {
+    const friendSignal = JSON.parse($('#txtFriendSignal').val());
+    p.signal(friendSignal);
+})
+console.log('Hello World');
